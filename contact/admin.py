0c69523048adc5dc8c7c15d592b9560d10216d7d
galihpujiirianto <galihpujiirianto@gmail.com>
2023-01-18 21:02:56 +0700

    Initialize model with ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class AdminContact(admin.ModelAdmin):
+    list_display = ['name', 'phone_number']
+    search_fields = ['name']
+
+admin.site.register(models.Contact, AdminContact)
+
