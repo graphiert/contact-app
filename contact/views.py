c19161b646be5d03af5477d179884f507fd07698
galihpujiirianto <galihpujiirianto@gmail.com>
2023-01-18 21:11:28 +0700

    Test viewing data from model in to view

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def index(request):
+    contacts = models.Contact.objects.all()
     ctx = {
         'pagetitle': 'Halaman Utama',
+        'contacts': contacts,
     }
     
     return render(request, 'contact/index.html', ctx)
